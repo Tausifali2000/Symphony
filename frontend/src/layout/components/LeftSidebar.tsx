09f6013fb04197a69e31bb8e4f58f39bb69f0c15
TausifAli2000 <tausifali2000@gmail.com>
2025-04-06 13:54:34 +0530

    Button Changes

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -58,7 +58,7 @@ const LeftSidebar = () => {
 				<div className='flex items-center justify-between mb-4'>
 					<div className='flex items-center text-white px-2'>
 						<Library className='size-5 mr-2' />
-						<span className='hidden md:inline'>Playlists</span>
+						<span className='hidden md:inline'>Albums</span>
 					</div>
 				</div>
 
